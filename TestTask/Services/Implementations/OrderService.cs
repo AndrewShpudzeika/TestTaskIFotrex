3cc1b5a604911b8dca2967724585f295d0cf39ea
mrkaramicrosoft@gmail.com <mrkaramicrosoft@gmail.com>
2024-02-15 13:14:31 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+	public class OrderService : IOrderService
+	{
+		ApplicationDbContext _context;
+
+		public OrderService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<Order> GetOrder()
+		{
+			var orderWithMaxPrice = await _context.Orders.
+				OrderByDescending(u => u.Price).FirstOrDefaultAsync();
+
+			return orderWithMaxPrice;
+		}
+
+		public async Task<List<Order>> GetOrders()
+		{
+			var ordersWithValue = await _context.Orders.
+				Where(u => u.Quantity > 10).ToListAsync();
+
+			return ordersWithValue;
+		}
+	}
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+	public class UserService : IUserService
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public UserService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<User> GetUser()
+		{
+			var userWithMostOrders = await _context.Users
+				.Where(u => u.Status == UserStatus.Active)
+				.OrderByDescending(u => u.Orders.Sum(o => o.Quantity))
+				.Include(u => u.Orders)
+				.FirstOrDefaultAsync();
+
+			return userWithMostOrders;
+		}
+
+		public async Task<List<User>> GetUsers()
+		{
+			var inactiveUsers = await _context.Users
+				.Include(u => u.Orders)
+				.Where(u => u.Status == UserStatus.Inactive)
+				.ToListAsync();
+
+			return inactiveUsers;
+		}
+	}
+}
